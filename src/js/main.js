0ce86d9cf8c486bfbc85eb0448d563d8bf28a6c3
Saulo-Fero-Maciel <kogasaal@gmail.com>
2025-03-05 01:40:51 -0300

    Last Update 	modified:   index.html 	modified:   src/css/reset.css 	modified:   src/css/style.css 	modified:   src/js/main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,19 @@
 
+// Efeito dos botões de brilhar
+const buttons = document.querySelectorAll('.button');
+  
+buttons.forEach(button => {
+    button.addEventListener('click', function() {
+    this.classList.add('clicked');
+    setTimeout(() => {
+        this.classList.remove('clicked');
+    }, 150);
+    });
+
+    button.addEventListener('mouseover', function() {
+    this.classList.add('clicked');
+    setTimeout(() => {
+        this.classList.remove('clicked');
+    }, 180);
+    });
+});
