5e14301f9792bc02b8336e763bceef3bb1a57b8f
Saulo-Ferro-Maciel <kogasaal@gmail.com>
2025-03-17 00:36:42 -0300

    Last Update in: 	modified:   src/css/responsive.css     modified:   src/css/style.css 	modified:   src/js/main.js

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,8 @@ document.addEventListener("DOMContentLoaded", () => {
         "https://images.unsplash.com/photo-1511632765486-a01980e01a18",
         "https://images.unsplash.com/photo-1556817411-31ae72fa3ea0",
         "https://images.unsplash.com/photo-1517649763962-0c623066013b",
-        "https://images.unsplash.com/photo-1554980555-7afb7c8795fe"
+        "https://images.unsplash.com/photo-1554980555-7afb7c8795fe",
+        "https://images.unsplash.com/photo-1545389336-cf090694435e"
     ];
 
     const imagesDesktop = [
